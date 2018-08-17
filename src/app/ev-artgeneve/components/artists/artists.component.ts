b784c3cead5899e73fa433c8bf9609f11e4a53aa
oscar <ozotto@hotmail.com>
2018-08-17 15:01:28 +0200

    add material table in artists

--- src/app/ev-artgeneve/components/artists/artists.component.ts
+++ src/app/ev-artgeneve/components/artists/artists.component.ts
@@ -1,68 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import { Router } from '@angular/router';
 
-import { ArtistService } from '../../services/artist.service';
-import { Artist } from '../../models/artist';
+import {Observable} from 'rxjs';
+import {SelectionModel} from '@angular/cdk/collections';
 
-import { LocalDataSource } from 'ng2-smart-table';
-import * as tableData from '../data/data-artists-rep';
-//import { BasicExampleLoadService } from '../data/basic-example-load.service';
+import {ArtistService} from '../../services/artist.service';
+import {Artist} from '../../models/artist';
 
 import * as moment from 'moment';
 import * as _ from 'lodash';
 
 @Component({
   selector: 'app-artists',
+  providers: [ArtistService],
   templateUrl: './artists.component.html',
   styleUrls: ['./artists.component.scss']
 })
 export class ArtistsComponent implements OnInit {
 
-  settings = tableData.settings;
-  source: LocalDataSource;
+  displayedColumns: string[] = ['select', 'actions', 'id', 'exhibitor', 'artist', 'state'];
+  dataSource = new MatTableDataSource();
+  selection = new SelectionModel(true, []);
+ 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  public path: any[];
-
+  private path: any[];
   private isExhibited: Boolean;
-  
+
   constructor(
     private router : Router,
     private artistService: ArtistService,
 	) {
     this.path = this.router.url.split("/");
     this.isExhibited = (this.path[2] == 'art-exhibited') ? true : false;
-   
   }
 
-
-
   ngOnInit() {
+    this.configureDataSource()
     this.getArtists();
   }
 
   getArtists(): any {
-    this.artistService.getArtists()
-      .subscribe(artists => {
-        if(this.isExhibited){
-          
-          this.source = new LocalDataSource( _.filter(artists, art => { 
-            if(art.is_exhibited == true) return art; 
-          }) );
-
-        }else{
-          this.source = new LocalDataSource( _.filter(artists, art => { 
-            if(art.is_exhibited == false) return art; 
-          }) );
-          /*this.source = new LocalDataSource(artists);*/
-        }
+    this.artistService.getArtists().subscribe(artists => {
+      if(this.isExhibited){
+        this.dataSource.data = _.filter(artists, art => { 
+          if(art.is_exhibited == true) return art; 
+        }) ;
+      }else{
+        this.dataSource.data = _.filter(artists, art => { 
+          if(art.is_exhibited == false) return art; 
+        }) ;
+      }
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
         
-      }); 
+    }); 
   }
 
-  selectedArtist: Artist;
+  configureDataSource() {
+    /* configure filter */
+    this.dataSource.filterPredicate = (data: Artwork, filter: string) => 
+      data.exhibitor.cat_banner.trim().toLowerCase().indexOf(filter) != -1 || data.title.trim().toLowerCase().indexOf(filter) != -1 ||
+      data.artist.first_name.trim().toLowerCase().indexOf(filter) != -1 || data.artist.last_name.trim().toLowerCase().indexOf(filter) != -1 ;
+    
+    /* configure sort */
+    this.dataSource.sortingDataAccessor = (data: Artwork, property) => {
+      switch(property) {
+        case 'exhibitor': return data.exhibitor.cat_banner;
+        case 'artist': return data.artist.first_name;
+        case 'state': return data.state.title;
+        default: return data[property];
+      }
+    };
+  }
 
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  applyFilter(filterValue: string) { 
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  /*selectedArtist: Artist;
 	onSelectArtist(artist: Artist): void {
 	  this.selectedArtist = artist;
-	}
+	}*/
 
 }
