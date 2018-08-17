88041d0875439dffcf51f6b5dba2c53ad01c7dc7
oscar <ozotto@hotmail.com>
2018-08-17 21:48:33 +0200

    add detail artist

--- src/app/ev-artgeneve/components/artists/artist-detail/artist-detail.component.ts
+++ src/app/ev-artgeneve/components/artists/artist-detail/artist-detail.component.ts
@@ -1,5 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {MatInputModule, MatFormFieldControl} from '@angular/material';
+
+import * as _ from 'lodash';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {FormControl, FormBuilder, FormGroup} from '@angular/forms';
+
+import {ArtistService} from '../../../services/artist.service';
+import {ExhibitorService} from '../../../services/exhibitor.service';
+
+import {Artist} from '../../../models/artist';
+import {Exhibitor} from '../../../models/exhibitor';
+import {State} from '../../../models/state';
+import {StateValues} from '../../../models/state';
 
 @Component({
   selector: 'app-artist-detail',
@@ -10,13 +26,82 @@ export class ArtistDetailComponent implements OnInit {
 
 	private path: any[];
   private isExhibited: Boolean;
+  
+  private artist: Artist;
+  private isNew: Boolean;
 
-  constructor(private router : Router) {
+  defaultExhibitors: Array<Exhibitor>;
+  exhibitorFormControl: FormControl;
+  filteredExhibitors: any;
+
+  selectedValue: number;
+  states: State[] = StateValues;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router : Router,
+    private location: Location,
+    private artistService: ArtistService,
+    private exhibitorService: ExhibitorService,
+  ) {
+
   	this.path = this.router.url.split("/");
     this.isExhibited = (this.path[2] == 'art-exhibited') ? true : false;
+    this.route.params.subscribe( params => this.isNew = _.isEmpty(params) );
+
+    this.artist = new Artist();
+    this.exhibitorFormControl = new FormControl();
   }
 
   ngOnInit() {
+    
+    if(!this.isNew) {
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.getArtist(id);
+    }
+
+    this.getExhibitors();
+
+  }
+
+  getArtist(id): void {
+    this.artistService.getArtist(id).subscribe(artist => {
+      this.artist = artist;
+      this.selectedValue = this.artist.state.id;
+    });
+  }
+
+  getExhibitors(): any {
+    this.exhibitorService.getExhibitors().subscribe(exhibitors => {
+      
+      this.defaultExhibitors = exhibitors;
+      
+      this.exhibitorFormControl.valueChanges.pipe(
+        startWith(null),
+        map(value => this.filterExhibitor(value)))
+        .subscribe(exhibitorsFiltered => {
+          this.filteredExhibitors = exhibitorsFiltered;
+        });
+
+      this.exhibitorFormControl.setValue(this.artist.exhibitor.cat_banner)
+
+    }); 
+
+  }
+
+  filterExhibitor(val: string): Exhibitor[] {
+    var filteredExh = _.filter(this.defaultExhibitors, exhibitor => { 
+      return exhibitor.cat_banner.trim().toLowerCase().indexOf(val) != -1; 
+    });
+    return val ? filteredExh : this.defaultExhibitors;
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    console.log('test')
   }
 
 }
