0319aecabc4422f4394a3daf40599953cdd3d929
oscar <ozotto@hotmail.com>
2018-11-01 09:48:42 +0100

    change host back

--- src/app/sys/constants.ts
+++ src/app/sys/constants.ts
@@ -1,2 +1,3 @@
-export const BACKEND_URL = 'http://newexhibitorscatalog.palexpo.ch:8000';
+//export const BACKEND_URL = 'http://newexhibitorscatalog.palexpo.ch:8000';
+export const BACKEND_URL = 'http://127.0.0.1:8000';
 export const API_URL = 'http://api.palexpo.ch:8443';
